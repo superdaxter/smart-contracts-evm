9a9a74c7418c57be8916a8ce4ec89d1e68405cd6
Swift <swift@pop-os.localdomain>
2021-12-24 14:23:16 -0700

    first commit

---
+++ deploy.py
@@ -0,0 +1,114 @@
+from solcx import compile_standard, install_solc
+import json
+from web3 import Web3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+with open("SimpleStorage.sol", "r") as file:
+    lines = file.read()
+
+install_solc("0.6.0")
+
+print("Compiled Solidity!")
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": lines}},
+        "settings": {
+            "outputSelection": {
+                "*": {"*": ["abi", "metadata", "evm.bytecode", "evm.sourceMap"]}
+            }
+        },
+    },
+    solc_version="0.6.0",
+)
+
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
+
+# deploying smart contract
+print("Deploying Contract...")
+# get the bytecode
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+
+print("Contract Deployed!")
+
+# get abi
+abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+
+
+# connect to ganache test network
+w3 = Web3(
+    Web3.HTTPProvider("https://rinkeby.infura.io/v3/4bcc1212c74741199bee1ed7713b3254")
+)
+chain_id = 4
+my_address = "0xd2d4bb8FF3187D0bcF7966bCe8A8C76A72F3691c"
+# PRIVATE_KEY in .env file
+private_key = os.getenv("PRIVATE_KEY")
+
+# Create contract
+print("Create Contract...")
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+print("Contract Created!")
+# Get nonce from latest transaction
+nonce = w3.eth.getTransactionCount(my_address)
+
+# build
+transaction = SimpleStorage.constructor().buildTransaction(
+    {
+        "chainId": chain_id,
+        "from": my_address,
+        "nonce": nonce,
+        "gasPrice": w3.eth.gas_price,
+    }
+)
+
+# sign
+singed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+
+# send
+tx_hash = w3.eth.send_raw_transaction(singed_txn.rawTransaction)
+# wait for confirmations
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+
+
+# working with contract
+# Contract Address
+# Contract ABI
+
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+# 2 ways to interact
+# Call       -> do not make state change
+# Transact   -> make a state change
+
+# print retrieve function call from simple_storage
+print(simple_storage.functions.retrieve().call())
+
+print("Updating Contract...")
+# create method to store
+store_transaction = simple_storage.functions.store(79).buildTransaction(
+    {
+        "chainId": chain_id,
+        "from": my_address,
+        "nonce": nonce + 1,
+        "gasPrice": w3.eth.gas_price,
+    }
+)
+
+# sign txn
+singed_store_txn = w3.eth.account.sign_transaction(
+    store_transaction, private_key=private_key
+)
+
+# send transaction
+send_store_txn = w3.eth.send_raw_transaction(singed_store_txn.rawTransaction)
+receipt_store_txn = w3.eth.wait_for_transaction_receipt(send_store_txn)
+
+print("Contract Updated!")
+
+# print retrieve function call from simple_storage again (changed)
+print(simple_storage.functions.retrieve().call())
---
+++ links.py
@@ -0,0 +1,5 @@
+# Connect to Mainet/Testnet
+# https://infura.io/
+
+# Get Chaind ID for EVM network
+# https://chainlist.org/
